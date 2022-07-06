9884775111b714bf5cf2d319c868d8518d5f6c45
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-06 15:44:29 -0500

    Clase 115

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,41 @@
+import { useState } from 'react'
+
 import LoginStyles from './Login.module.css'
 
 export const Login = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <form
+      onSubmit={handleSubmit}
+      className={LoginStyles['login-form']}
+    >
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+          autoComplete="current-email"
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          autoComplete="current-password"
+        />
+      </label>
+      <button className='btn'>Login!</button>
+    </form>
   )
 }
