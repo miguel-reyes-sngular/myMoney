f61092aa823207d8809a60d8690d8a37ef14c648
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-07 16:17:07 -0500

    Clase 124

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 import navbarStyles from './Navbar.module.css'
 
 export const Navbar = () => {
+  const { logout } = useLogout()
+
   return (
     <nav className={navbarStyles.navbar}>
       <ul>
         <li className={navbarStyles.title}>myMoney</li>
         <li><Link to='/login'>Login</Link></li>
         <li><Link to='/signup'>Signup</Link></li>
+        <li>
+          <button
+            className='btn'
+            onClick={logout}
+          >
+            Logout
+          </button>
+        </li>
       </ul>
     </nav>
   )
