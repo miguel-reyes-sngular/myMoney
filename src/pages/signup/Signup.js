770e2c174c7a988a5caf9482185af63c05d9b4cf
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-07 13:32:17 -0500

    Clase 120

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-
+import { useSignup } from '../../hooks/useSignup'
 import SignupStyles from './Signup.module.css'
 
 export const Signup = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
+  const { signup, isPending, error } = useSignup()
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email, password, displayName)
+    signup(email, password, displayName)
   }
 
   return (
@@ -43,7 +45,9 @@ export const Signup = () => {
           autoComplete="current-password"
         />
       </label>
-      <button className='btn'>Signup!</button>
+      { !isPending && <button className='btn'>Signup!</button> }
+      { isPending && <button className='btn' disabled>Loading...</button> }
+      { error && <p>{error}</p> }
     </form>
   )
 }
