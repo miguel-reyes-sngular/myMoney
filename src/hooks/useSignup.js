6523953c0974420e530f95d1a6ff4ac94bba4403
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-07 11:54:36 -0500

    Clase 119

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { projectAuth } from '../firebase/config'
+
+export const useSignup = () => {
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+
+  const signup = async (email, password, displayName) => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      //* signup user
+      const response = await projectAuth.createUserWithEmailAndPassword(email, password)
+      console.log(response.user);
+
+      if (!response) {
+        throw new Error('Could not complete signup');
+      }
+
+      //* add display name to user
+      await response.user.updateProfile({ displayName })
+
+      setIsPending(false)
+      setError(null)
+    } catch (error) {
+      console.error('There was an error: ', error);
+      setError(error.message)
+      setIsPending(false)
+    }
+  }
+
+  return {
+    error,
+    isPending,
+    signup,
+  }
+
+}
