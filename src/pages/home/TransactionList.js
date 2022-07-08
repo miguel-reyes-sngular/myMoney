317cc939e6cca373691282906f059b53ecc7c36b
@miguel.reyes_sngular <miguel.reyes@sngular.com>
2022-07-08 17:56:27 -0500

    clase 142

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,7 +1,11 @@
+import { useFirestore } from '../../hooks/useFirestore'
+
 import homeStyles from './Home.module.css'
 
 export const TransactionList = props => {
   const { transactions } = props
+
+  const { deleteDocument } = useFirestore('transactions')
 
   return (
     <ul className={homeStyles.transactions}>
@@ -13,6 +17,11 @@ export const TransactionList = props => {
           <p className={homeStyles.amount}>
             ${transaction.amount}
           </p>
+          <button
+            onClick={() => deleteDocument(transaction.id)}
+          >
+            X
+          </button>
         </li>
       ))}
     </ul>
